9bc3d5245c3090949ddd4d3a616c573c50a44061
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:13:57 -0500

    Use `contextlib`'s `suppress` method to silence an error

--- modules/reddit.py
+++ modules/reddit.py
@@ -16,11 +16,8 @@ def reddit(username):
 
     with contextlib.suppress(Exception):
         about = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]/div/div[1]/div/div[3]').get_attribute("innerHTML")
-    try:
+    with contextlib.suppress(Exception):
         link = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]/div/div[1]/div/div[5]').get_attribute("innerHTML")
-    except Exception:
-        pass
-
     return {
         "about": about,
         "link": link
