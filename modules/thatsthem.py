fa67820414196f71a65730277fee82884b94d922
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:03:21 -0500

    Use `except Exception:` rather than bare `except:`

--- modules/thatsthem.py
+++ modules/thatsthem.py
@@ -124,7 +124,7 @@ def thatsthem(username, method="email"):
                 try:
                     location = driver.find_element(By.XPATH,
                                                    f'/html/body/div/main/div/div[2]/div[2]/div[2]/div[{i}]/div/div[1]/div[3]').text
-                except:
+                except Exception:
                     location = None
                 try:
                     gender = driver.find_element(By.XPATH,
