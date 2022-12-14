93cc426f0460dde8cbc50809d3c659089ed1dbfc
livxy <67598470+livxy@users.noreply.github.com>
2022-12-14 14:19:55 -0500

    Replace call to format with f-string

--- modules/phone_search.py
+++ modules/phone_search.py
@@ -3,7 +3,7 @@ from ScrapeSearchEngine.SearchEngine import *
 
 def search_phone(number):
     results = []
-    search = ("\"{}\"".format(number))
+    search = f'\"{number}\"'
     try:
         googleText, googleLink = Google(search=search,
                                         userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.96 Safari/537.36")
