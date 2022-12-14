6603bd4ae67a58ecab19e764f277f6dd29d5da61
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:07:14 -0500

    Use `except Exception:` rather than bare `except:`

--- modules/twitter.py
+++ modules/twitter.py
@@ -25,7 +25,7 @@ def twitter(username):
         link = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[4]/div/a').get_attribute('href')
     try:
         name = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/div/div/span[1]/span').text
-    except:
+    except Exception:
         pass
     try:
         birthday = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[4]/div/span[1]').text
