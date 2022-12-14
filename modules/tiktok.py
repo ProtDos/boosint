1f2ffbc547244893cf8e0543d7fe790fc297ad7b
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:04:40 -0500

    Use `except Exception:` rather than bare `except:`

--- modules/tiktok.py
+++ modules/tiktok.py
@@ -16,7 +16,7 @@ def tiktok(username):
 
     try:
         about = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[1]/h2[2]').text
-    except:
+    except Exception:
         pass
     try:
         link = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[1]/div[2]/a').text
