5f3d7efbd90f9e5504d88c4c4ec5874fdf441e67
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:13:19 -0500

    Use `except Exception:` rather than bare `except:`

--- modules/reddit.py
+++ modules/reddit.py
@@ -15,7 +15,7 @@ def reddit(username):
 
     try:
         about = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]/div/div[1]/div/div[3]').get_attribute("innerHTML")
-    except:
+    except Exception:
         pass
     try:
         link = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]/div/div[1]/div/div[5]').get_attribute("innerHTML")
