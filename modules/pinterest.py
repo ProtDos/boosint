efef6d655b156116048f2ecc76b989efeb99f27b
livxy <67598470+livxy@users.noreply.github.com>
2022-12-13 21:14:23 -0500

    Use `contextlib`'s `suppress` method to silence an error

--- modules/pinterest.py
+++ modules/pinterest.py
@@ -1,3 +1,4 @@
+import contextlib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -14,10 +15,8 @@ def pin(username):
     driver.get(f"https://pinterest.com/{username}")
 
     time.sleep(5)
-    try:
+    with contextlib.suppress(Exception):
         about = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[1]/div[1]/div/div[4]/div/div/div/div/div/span').get_attribute("innerHTML")
-    except Exception:
-        pass
     try:
         name = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/div/h1/div').text
     except:
